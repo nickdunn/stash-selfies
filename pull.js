ccec491c15395c8702f41b21ad0e225906232e3a
Nick Dunn <nick@nick-dunn.co.uk>
2014-10-09 22:16:59 +0100

    Initial commit

---
+++ pull.js
@@ -0,0 +1,45 @@
+(function() {
+
+  var config = {
+      insertBefore : [
+        'button:contains(Comment)'
+      ],
+      bodySelector : 'textarea[name="text"]',
+      buttonHTML   : (
+        '<button id="totallyAwesomeSelfieButton" type="button" class="button">' +
+          '<span id="totallyAwesomeSelfieIcon" class="octicon octicon-device-camera"></span>' +
+          'Selfie!' +
+        '</button>'
+      ),
+      placeVideo     : function (video) { $('.comment-form-footer').prepend(video); },
+      preVideoStart  : function () { $('.comment-form-footer').addClass('expandSome'); },
+      postVideoStart : function () {
+        $('.comment-form-footer')
+          .removeClass('expandSome')
+          .addClass('expand');
+      },
+      postVideoStop : function () { $('.comment-form-footer').removeClass('expand'); }
+
+    }
+    , client = new GitHubSelfies(config);
+
+  client.setupSelfieStream();
+
+  // if the user submits a comment, we need to add everything to the next one
+  // var trySetupAgain = function() {
+  //   if (client.setupComplete && $('#totallyAwesomeSelfieButton').length === 0) {
+  //     client.setupSelfieStream();
+  //   }
+  // };
+  // setInterval(trySetupAgain, 500);
+
+  // github hides instead of deleting dom elements when you submit
+  // var cleanup = function() {
+  //   $('#totallyAwesomeSelfieButton').remove();
+  //   $('#selfieVideo').remove();
+  //   $('#selfieCanvas').remove();
+  // };
+
+  // $('[name="comment_and_open"]').on('click', cleanup);
+  // $('[name="comment_and_close"]').on('click', cleanup);
+})();
